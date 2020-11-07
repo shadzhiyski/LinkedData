9bab5d9c164a6591c5fa31502b8f50229e991c1c
Stoyan Hadzhiyski <stoian2662@gmail.com>
2020-11-07 12:50:39 +0200

    rest_service.py updated.

---
+++ LinkedData.Ontology.RestService/DmtoRepository.py
@@ -0,0 +1,86 @@
+from lxml import objectify, etree as ET
+import uuid
+
+class DmtoRepository():
+    def __init__(self, ontology_path):
+        self._ontology_path = ontology_path
+        self._ontology_xml = ET.parse(ontology_path).getroot()
+
+    def add_patient(self):
+	    return self._add_individual(class_name='#DMTO_0000021', prefix='#PATIENT')
+
+    def add_patient_profile(self):
+        return self._add_individual(class_name='#DMTO_0001670', prefix='#PATIENT_PROFILE')
+
+    def add_lab_test(self, class_name, test_name):
+        return self._add_individual(class_name=class_name, prefix=F'#LAB_TEST_{test_name}', is_abbreviated=True)
+
+    def add_treatment_plan(self):
+        return self._add_individual(class_name='#DMTO_0000044', prefix='#TREATMENT_PLAN')
+
+    def add_lifestyle_subplan(self):
+        return self._add_individual(class_name='#DMTO_0001712', prefix='#LIFESTYLE_SUBPLAN')
+
+    def add_diet(self):
+        return self._add_individual(class_name='#DMTO_0001704', prefix='#DIET')
+
+    def add_breakfast_meal(self):
+        return self._add_individual(class_name='Onto:meal', prefix='#BREAKFAST_MEAL', is_abbreviated=True)
+
+    def add_relation(self, named_individual_owner, named_individual_referenced, property_name, is_abbreviated=False):
+        object_property_assertion = ET.Element('ObjectPropertyAssertion')
+
+        if is_abbreviated:
+            object_property_assertion.append(ET.Element('ObjectProperty', abbreviatedIRI=F'{property_name}'))
+        else:
+            object_property_assertion.append(ET.Element('ObjectProperty', IRI=F'{property_name}'))
+
+        object_property_assertion.append(ET.Element('NamedIndividual', IRI=named_individual_owner))
+        object_property_assertion.append(ET.Element('NamedIndividual', IRI=named_individual_referenced))
+
+        self._ontology_xml.append(object_property_assertion)
+
+        return object_property_assertion
+
+    def add_data_property(self, named_individual_owner, property_name, data_type, data_value, is_abbreviated=False):
+        object_property_assertion = ET.Element('DataPropertyAssertion')
+
+        if is_abbreviated:
+            object_property_assertion.append(ET.Element('DataProperty', abbreviatedIRI=F'{property_name}'))
+        else:
+            object_property_assertion.append(ET.Element('DataProperty', IRI=F'{property_name}'))
+
+        object_property_assertion.append(ET.Element('NamedIndividual', IRI=named_individual_owner))
+
+        literal = ET.Element('Literal', datatypeIRI=data_type)
+        literal.text = data_value
+        object_property_assertion.append(literal)
+
+        self._ontology_xml.append(object_property_assertion)
+
+        return object_property_assertion
+
+    def save(self):
+        self._ontology_xml[:] = sorted(self._ontology_xml, key=lambda x: 'A' if x.tag.endswith('Prefix') else x.tag)
+
+        et = ET.ElementTree(self._ontology_xml)
+        et.write(self._ontology_path, pretty_print=True)
+
+    def _add_individual(self, class_name, prefix, is_abbreviated=False):
+        declaration = ET.Element('Declaration')
+        class_assertion = ET.Element('ClassAssertion')
+
+        if is_abbreviated:
+            class_assertion.append(ET.Element('Class', abbreviatedIRI=F'{class_name}'))
+        else:
+            class_assertion.append(ET.Element('Class', IRI=F'{class_name}'))
+
+        id = uuid.uuid4()
+        individual_name = F'{prefix}_{id}'
+        declaration.append(ET.Element('NamedIndividual', IRI=individual_name))
+        class_assertion.append(ET.Element('NamedIndividual', IRI=individual_name))
+
+        self._ontology_xml.append(declaration)
+        self._ontology_xml.append(class_assertion)
+
+        return individual_name
--- LinkedData.Ontology.RestService/rest_service.py
+++ LinkedData.Ontology.RestService/rest_service.py
@@ -6,173 +6,68 @@ from flask import jsonify
 from flask import abort
 from flask_expects_json import expects_json
 
+from lxml import objectify, etree as ET
+
+from DmtoRepository import DmtoRepository
+
 app = Flask(__name__, template_folder='templates')
 app.debug = True
 app.config['SERVER_NAME'] = 'localhost:5555'
 
-from OntoRepository import OntoRepository
+lab_tests_map = {
+	'TotalCholesterol': 'DDO.:DDO_0000274',
+	'Glucose': 'DDO.:DDO_0000244',
+	'UricAcid': 'DDO.:DDO_0000296'
+}
 
-from owlready2 import *
-onto_path.append("Repository")
-onto = get_ontology('file://Repository/DMTO.owl')
-onto.load()
+def import_patients_data(ontology_repo, patients_xml):
+	patients = []
+	for input_amb_list in patients_xml.Doctor.AmbList:
+		patient = ontology_repo.add_patient()
+		patient_profile = ontology_repo.add_patient_profile()
+		treatment_plan = ontology_repo.add_treatment_plan()
+		lifestyle_subplan = ontology_repo.add_lifestyle_subplan()
+		diet = ontology_repo.add_diet()
+		breakfast_meal = ontology_repo.add_breakfast_meal()
 
-from json_utils import *
+		ontology_repo.add_data_property(
+			patient_profile,
+			'https://bioportal.bioontology.org/ontologies/DTO.owl#DTO:0002111',
+			'http://www.w3.org/2001/XMLSchema#double',
+			'1700',
+		)
 
-from models import load_models
-load_models(onto)
+		ontology_repo.add_relation(patient, patient_profile, '#DMTO_0001667')
+		ontology_repo.add_relation(patient_profile, treatment_plan, '#DMTO_0001671')
+		ontology_repo.add_relation(treatment_plan, lifestyle_subplan, '#DMTO_0001701')
+		ontology_repo.add_relation(lifestyle_subplan, diet, 'https://bioportal.bioontology.org/ontologies/DTO.owl#DTO:0001953')
+		ontology_repo.add_relation(diet, breakfast_meal, 'https://bioportal.bioontology.org/ontologies/DTO.owl#DTO:0001972')
 
-repo = OntoRepository(onto)
+		anamnesa = input_amb_list.Anamnesa
+		for lab_test_label, lab_test_class in lab_tests_map.items():
+			lab_test = ontology_repo.add_lab_test(lab_test_class, lab_test_label)
 
-@app.route('/ontology/diet/suggest', methods=['GET'])
-def suggest_diet():
-	abort(501)
-	# TODO: implement diets suggestion
-	...
+			value = anamnesa['Blood'][lab_test_label].text
+			ontology_repo.add_data_property(lab_test, 'DDO.:DDO_0000134', 'http://www.w3.org/2001/XMLSchema#float', value, True)
 
-@app.route('/ontology/diet', methods=['GET'])
-def get_diet():
-	
-	title = request.args.get('title')
+			ontology_repo.add_relation(patient_profile, lab_test, 'DDO.:DDO_0000311', True)
 
-	if not title:
-		abort(400)
-	
-	diet_onto = repo.search_one(is_a=onto.Diet, title=title)
+		patients.append(patient)
 
-	if not diet_onto:
-		abort(404)
-
-	return jsonify(diet_to_json(diet_onto))
-
-@app.route('/ontology/diet', methods=['POST'])
-@expects_json(DIET_SCHEMA)
-def create_diet():
-
-	diet_json = request.json
-
-	deseases = [desease for desease in [repo.search_one(title=desease) \
-			for desease in diet_json['deseases']
-		] if desease is not None
-	]
-
-	foods = [food for food in [
-			repo.search_one(iri=F'*{food}') for food in diet_json['foods']
-		] if food is not None
-	]
-
-	id, diet_onto = repo.create(class_name='Diet')
-
-	diet_onto.title = diet_json['title']
-	diet_onto.has_desease = deseases
-	diet_onto.has_food = foods
-
-	repo.save()
-
-	return jsonify({
-		'id': F'{id}',
-		'diet': diet_to_json(diet_onto)
-	})
-
-@app.route('/ontology/desease', methods=['GET'])
-def get_desease():
-	
-	title = request.args.get('title')
-
-	if not title:
-		abort(400)
-	
-	desease_onto = repo.search_one(is_a=onto.Desease, title=title)
-
-	if not desease_onto:
-		abort(404)
-
-	return jsonify(desease_to_json(desease_onto))
-
-@app.route('/ontology/desease', methods=['POST'])
-@expects_json(DESEASE_SCHEMA)
-def create_desease():
-
-	desease_json = request.json
-
-	symptoms = [repo.search_one(label=symptom) \
-		for symptom in desease_json['symptoms']
-	]
-
-	id, desease_onto = repo.create(class_name='Desease')
-
-	desease_onto.title = desease_json['title']
-	desease_onto.has_symptom = symptoms
-
-	repo.save()
-
-	return jsonify({
-		'id': F'{id}',
-		'desease': desease_to_json(desease_onto)
-	})
-
-from lxml import objectify
-import lxml
-
-from flask import Response
-@app.route('/ontology/patient', methods=['GET'])
-def get_patient():
-	patient_profile_id = request.args.get('patient_profile_id')
-
-	if not patient_profile_id:
-		abort(400)
-
-	repo.search_one(is_a=onto.DMTO_0001670, iri=F'*{patient_profile_id}')
-
-	return jsonify({ 'reference_id': str(patient_profile_id) })
+	return patients
 
 @app.route('/ontology/patient/import', methods=['POST'])
 def import_patients():
-	patient_xml_content = request.data
-	root = objectify.fromstring(patient_xml_content)
+	patients_xml_content = request.data
+	patients_xml = objectify.fromstring(patients_xml_content)
 
-	mappings = {
-		'Lymph': 'DDO_0000259',
-		'Mid': 'DDO_0000260',
-		'MCH': 'DDO_0000255',
-		'MCHC': 'DDO_0000256',
-		'MCV': 'DDO_0000254',
-		'HCT': 'DDO_0000253',
-		'RBC': 'DDO_0000251',
-		
-		'TotalCholesterol': 'DDO_0000274',
+	dmto_ontology_repo = DmtoRepository('Repository/DMTO_full_fixed.owl')
 
-		'HGB': 'DDO_0009214'
-	}
+	patients = import_patients_data(dmto_ontology_repo, patients_xml)
 
-	for input_amb_list in root.Doctor.AmbList:
-		id, patient_profile = repo.create(
-			class_name='DMTO_0001670'
-		)
+	dmto_ontology_repo.save()
 
-		patient_profile.has_lab_test = []
-
-		input_anamnesa = input_amb_list.Anamnesa
-
-		# lab_tests = []
-		for name, cls_name in mappings.items():
-			item_id, item_instance = repo.create(
-				class_name='DDO_00906290',#cls_name,
-				namespace='http://purl.obolibrary.org/obo/DDO.owl'
-			)
-
-			item_instance.has_date = F'{input_amb_list.dataAl}T{input_amb_list.time}Z'
-			item_instance.has_quantitative_Value = input_anamnesa['Blood'][name].text
-
-			patient_profile.has_lab_test.append(item_instance)
-
-		# patient_profile.has_lab_test = lab_tests
-	
-	repo.save()
-
-	return jsonify({ 'reference_id': str(id) })
-	# jsonify([amb_list.Patient.EGN.text for amb_list in root.Doctor.AmbList])
-	# Response(str(root.Doctor.AmbList), mimetype='text/xml')
+	return jsonify({ 'patients_ids': patients })
 
 
 if __name__ == '__main__':
